9d3c04197a11cd0c620d6daedf74be9e146e5fd1
zhixuanlee <overlypositiveplayer@gmail.com>
2022-04-15 21:41:33 +0800

    Complete final version for submission

--- decodeMessage.m
+++ decodeMessage.m
@@ -7,8 +7,9 @@ function result = decodeMessage(embedded)
 
     for i = 1 : iterations
         process = embedded(1 + ((i - 1) * segmentLength) : i * segmentLength);
-        division = fix(length(process) / 20); % round towards to the nearest integer towards 0
-        chunk = process(1 + (7*division) : 15*division); % take the central portion of the signal (in time) for fft()
+        division = fix(length(process) / 20); % round to the nearest integer toward 0.
+        chunk = process(1 + (7*division) : 15*division); % take the central portion of the signal (in time) for fft().
+        fft_process = fft(chunk, 1400);
 
         mx = max(abs(fft_process));
         index = find(abs(fft_process) == mx);
@@ -19,7 +20,7 @@ function result = decodeMessage(embedded)
         else
             asciiValue = (freq - 16000) / dF;
             if (1 < asciiValue) && (asciiValue < 95)
-                result = result + char(asciiValue + 30); % add 30 back to obtain the unnormalised ascii value
+                result = result + char(asciiValue + 30); % add 30 to convert back to unnormalised ascii value.
             end
         end
     end
